180519b4b1af6d2525e982fb258eff6208559d63
Olga <olganikolsky@inbox.ru>
2024-09-06 21:57:36 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,66 @@
+function greet(name) {
+  console.log('Привет, ' + name + '!');
+}
+greet('Надежда');
+
+let multiply = function (a, b) {
+  return a * b;
+};
+console.log(multiply(2, 3));
+
+function isEven(n) {
+  if (n % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(isEven(6));
+
+const add = (a, b) => a + b;
+console.log(add(4, 6));
+
+const square = (k) => Math.pow(k, 2);
+console.log(square(8));
+
+const sayHello = (name) => console.log('Привет, ' + name + '!');
+sayHello('Тимур');
+
+function factorial(n) {
+  if (n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+console.log(factorial(5));
+
+function fibonacci(n) {
+  if (n <= 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+console.log(fibonacci(6)); // так и не поняла, как работают эти числа Фибоначчи, почему 1+0+1+1+0... выходит 5е число например, но поняла как работает рекурсия:)
+
+function sum(N) {
+  if (N === 1) {
+    return 1;
+  } else {
+    return N + sum(N - 1);
+  }
+}
+console.log(sum(10));
+
+function creatCounter() {
+  let count = 0;
+  return function () {
+    count++;
+    return count;
+  };
+}
+const counter = creatCounter();
+
+console.log(counter());
+console.log(counter());
+console.log(counter());
+
+//2 задач нет, вообще ничего не могу понять про замыкание
